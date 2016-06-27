f6fb3b0c2d84b8f0ee7ebc1f07bea68d58bf0310
Click2Cloud <kapil@click2cloud.net>
2016-06-27 21:05:41 +0530

    ip

--- src/ConnectionSetting.cs
+++ src/ConnectionSetting.cs
@@ -25,8 +25,18 @@ namespace Click2Cloud.Samples.AspNetCore.MvcMongoDb
     public static class ConnectionSetting
     {
         //TODO: Provide Cluster IP of mongo db server 
-        private static string mongoDBClusterIP = "172.30.127.244";
+        private static string mongoDBClusterIP
+         {
+            get
+            {
+                if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("MONGODB_SERVICE_HOST")))
+                {
+                    return Environment.GetEnvironmentVariable("MONGODB_SERVICE_HOST");
+                }
 
+                return string.Empty;
+            }
+        }
         internal static string CONNECTION_STRING
         {
             get
